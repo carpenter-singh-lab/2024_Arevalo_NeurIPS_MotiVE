d115722ec5abfa22ee4ba3ab3c1f3fed4baa9d09
John Arevalo <johnarevalo@gmail.com>
2025-02-03 10:55:28 -0500

    Add sql formater

--- query.sql
+++ query.sql
@@ -7,13 +7,13 @@ FROM read_parquet('./**/*metrics.parquet', filename=TRUE);
 
 
 SELECT split_part(filename, '/', 3) AS DATA,
- (success_at_15_num_target / success_at_15_pct_target)::INT AS total_gene,
- (success_at_15_num_source / success_at_15_pct_source)::INT AS total_cmpd,
- round(map_target, 3) AS map_gene,
- round(phenotypic_activity_target, 3) AS pa_gene,
- success_at_15_num_target AS gene_num,
- round(success_at_15_pct_target, 3) AS 'success@15',
- round(random_success_at_15_pct_target, 3) AS rnd_baseline,
+       (success_at_15_num_target / success_at_15_pct_target)::INT AS total_gene,
+       (success_at_15_num_source / success_at_15_pct_source)::INT AS total_cmpd,
+       round(map_target, 3) AS map_gene,
+       round(phenotypic_activity_target, 3) AS pa_gene,
+       success_at_15_num_target AS gene_num,
+       round(success_at_15_pct_target, 3) AS 'success@15',
+       round(random_success_at_15_pct_target, 3) AS rnd_baseline,
 FROM results
 WHERE infer_mode = 'cartesian'
 ORDER BY map_gene DESC;
